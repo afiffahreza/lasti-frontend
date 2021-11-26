2c1d88439d90a3f1dc7aa5430a24628288395537
afiffahreza <afiffahreza3@gmail.com>
2021-11-26 14:06:58 +0700

    Add detail home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Container, Grid, ListItem, Typography } from "@mui/material"
+import { Box } from "@mui/system"
 import React from "react"
 import Header from "../layout/Header"
 
@@ -5,7 +7,20 @@ const Home = () => {
     return (
         <div>
             <Header/>
-            Test
+            <Container maxWidth='lg' style={{marginTop: '20px'}}>
+                <Box sx={{ flexGrow: 1 }, {border: 1}, {bgcolor: "#B4C6A6"}}> 
+                    <Grid container spacing={1} style={{margin: "auto"}}>
+                        <Grid item xs={6} md={2}>
+                            <ListItem>Nama :</ListItem>
+                            <ListItem>Wallet :</ListItem>
+                        </Grid>
+                        <Grid item xs={6} md={10}>
+                            <ListItem>Orang hehe</ListItem>
+                            <ListItem>270700</ListItem>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
         </div>
     )
 }
