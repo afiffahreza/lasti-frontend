08577986500145574c7395a6fba3246516357309
afiffahreza <afiffahreza3@gmail.com>
2021-11-26 12:46:58 +0700

    Add route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Test from './pages/Test'
 import Login from './pages/login'
 import Signup from './pages/signup'
+import IsiWallet from './pages/IsiWallet'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,9 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route exact path={["/", "/signup"]} component={Signup}/>
+        <Route exact path={["/signup"]} component={Signup}/>
+        <Route exact path={["/login"]} component={Login}/>
+        <Route exact path={["/wallet"]} component={IsiWallet}/>
       </Switch>
     </div>
   )
